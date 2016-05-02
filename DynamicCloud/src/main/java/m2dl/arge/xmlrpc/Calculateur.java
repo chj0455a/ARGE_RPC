cb33b66ad423311999ad9fef6725877c864b2937
Tales of symphonia <Tales of symphonia>
2016-05-02 22:07:58 +0200

    ...

--- DynamicCloud/src/main/java/m2dl/arge/xmlrpc/Calculateur.java
+++ DynamicCloud/src/main/java/m2dl/arge/xmlrpc/Calculateur.java
@@ -2,10 +2,13 @@ package m2dl.arge.xmlrpc;
 
 
 
+import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
 import org.apache.xmlrpc.server.XmlRpcServer;
 import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
 import org.apache.xmlrpc.webserver.WebServer;
+
+import java.io.IOException;
 
 public class Calculateur {
 	public int add(int i1, int i2) throws InterruptedException {
@@ -24,20 +27,24 @@ public class Calculateur {
 		return i1 - i2;
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException, XmlRpcException {
 		if (args[0] != null) {
-			WebServer webServer = new WebServer(Integer.parseInt(args[0]));
+			try {
+				// Test
+				Integer.parseInt(args[0]);
 
-			XmlRpcServer xmlRpcServer = webServer.getXmlRpcServer();
+				WebServer webServer = new WebServer(Integer.parseInt(args[0]));
 
-			PropertyHandlerMapping phm = new PropertyHandlerMapping();
+				XmlRpcServer xmlRpcServer = webServer.getXmlRpcServer();
+
+				PropertyHandlerMapping phm = new PropertyHandlerMapping();
 			/*
 			 * Load handler definitions from a property file. The property file
 			 * might look like: Calculator=org.apache.xmlrpc.demo.Calculator
 			 * org.apache.xmlrpc.demo.proxy.Adder=org.apache.xmlrpc.demo.proxy.
 			 * AdderImpl
 			 */
-			phm.load(Thread.currentThread().getContextClassLoader(), "XmlRpcCalculator.properties");
+				phm.load(Thread.currentThread().getContextClassLoader(), "XmlRpcCalculator.properties");
 
 			/*
 			 * You may also provide the handler classes directly, like this:
@@ -46,14 +53,18 @@ public class Calculateur {
 			 * phm.addHandler(org.apache.xmlrpc.demo.proxy.Adder.class.getName()
 			 * , org.apache.xmlrpc.demo.proxy.AdderImpl.class);
 			 */
-			xmlRpcServer.setHandlerMapping(phm);
+				xmlRpcServer.setHandlerMapping(phm);
 
-			XmlRpcServerConfigImpl serverConfig = (XmlRpcServerConfigImpl) xmlRpcServer.getConfig();
-			serverConfig.setEnabledForExtensions(true);
-			serverConfig.setContentLengthOptional(false);
+				XmlRpcServerConfigImpl serverConfig = (XmlRpcServerConfigImpl) xmlRpcServer.getConfig();
+				serverConfig.setEnabledForExtensions(true);
+				serverConfig.setContentLengthOptional(false);
 
-			webServer.start();
-			System.out.println("Le Worker Node web a demarre ...");
+				webServer.start();
+				System.out.println("Le Worker Node web a demarre ...");
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				System.out.println("Mauvais argument, usage : \n./Calculateur <nombre entier>");
+			}
 		}
 	}
 }
