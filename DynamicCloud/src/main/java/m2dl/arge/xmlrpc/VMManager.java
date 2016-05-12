493b13204d9ad3b91640c3be0bf2f904d2cc7449
chj0455a <julien.chaloupy@master-developpement-logiciel.fr>
2016-05-12 17:40:59 +0200

    Unused code

--- DynamicCloud/src/main/java/m2dl/arge/xmlrpc/VMManager.java
+++ DynamicCloud/src/main/java/m2dl/arge/xmlrpc/VMManager.java
@@ -125,42 +125,14 @@ public class VMManager {
     private void cloudmipConnection() {
         LOGGER.info("Try to connect to cloudmip vm");
 	OSClient os = OSFactory.builder()
-	    .endpoint("http://195.220.53.45:5000/v2.0")
+	    .endpoint("http://195.220.53.61:5000/v2.0")
 	    .credentials("ens27","BTAAMU").tenantName("service")
 	    .authenticate();
         LOGGER.info("Connection succeed");
 	System.out.println(os);
 	System.out.println(os.images().list());
 
-		// Find all Users
-		List<? extends User> users = os.identity().users().list();
-System.out.println(users.toString());
-// List all Tenants
-		List<? extends Tenant> tenants = os.identity().tenants().list();
-        System.out.println(tenants.toString());
-// Find all Compute Flavors
-		List<? extends Flavor> flavors = os.compute().flavors().list();
-        System.out.println(flavors.toString());
-// Find all running Servers
-		List<? extends Server> servers = os.compute().servers().list();
-        System.out.println(servers.toString());
-// Suspend a Server
-//		os.compute().servers().action("serverId", Action.SUSPEND);
 
-// List all Networks
-		List<? extends Network> networks = os.networking().network().list();
-        System.out.println(networks.toString());
-// List all Subnets
-		List<? extends Subnet> subnets = os.networking().subnet().list();
-        System.out.println(subnets.toString());
-// List all Routers
-		List<? extends Router> routers = os.networking().router().list();
-        System.out.println(routers.toString());
-// List all Images (Glance)
-		List<? extends Image> images = os.images().list();
-        System.out.println(images.toString());
-// Download the Image Data
-		InputStream is = os.images().getAsStream("imageId");
     }
 
     public void augmenterLaCharge() {
