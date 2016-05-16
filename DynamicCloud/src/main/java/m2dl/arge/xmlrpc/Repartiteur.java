5da45a62790f2003169112f9710069fb5dc2f3e8
chj0455a <julien.chaloupy@master-developpement-logiciel.fr>
2016-05-16 18:18:28 +0200

    Debug calculateur

--- DynamicCloud/src/main/java/m2dl/arge/xmlrpc/Repartiteur.java
+++ DynamicCloud/src/main/java/m2dl/arge/xmlrpc/Repartiteur.java
@@ -142,7 +142,7 @@ public class Repartiteur {
                     ("------------------------------------------------------------------------------------------------------------------------------------------------- Transmission au calculateur " + calculateur.getAdresse() + ":" + calculateur.getPort() + " de charge " + calculateur.getCharge_courante());
             writer.println
                     ("------------------------------------------------------------------------------------------------------------------------------------------------- Transmission au calculateur " + calculateur.getAdresse() + ":" + calculateur.getPort() + " de charge " + calculateur.getCharge_courante());
-            result = (Integer) calculateur.getClient().execute("Calculateur.requette", params);
+            result = (Integer) calculateur.getClient().execute("Calculateur.requete", params);
 
             calcIndexLoadBalance = (calcIndexLoadBalance + 1) % calculateursLoadBalancing.size();
 
